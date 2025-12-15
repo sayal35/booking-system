b9d261dd43a76bf6d5ffb7a9a1c47b75784f2ad4
karki <karkisayal7@gmail.com>
2025-12-15 17:57:08 +0545

    get fixed

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const bookingController = require("../controllers/bookingController");
-const { get } = require("../app");
-
-router.post("/", bookingController.createBooking);
 
 router.get("/", bookingController.getBookings);
 
